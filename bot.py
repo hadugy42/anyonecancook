7932fd53f1f2d7b3836078b210195a2ebf3284b1
hadugy42 <135345272+hadugy42@users.noreply.github.com>
2023-06-02 19:44:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-
+from config import Config
 plugins = dict(
     root = "plugins"
 )
 
 app = Client(
     "Trading Bot",
-    api_id=123,
-    api_hash="dgsg",
-    bot_token="gdfhg",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
     plugins=plugins
 )
 
